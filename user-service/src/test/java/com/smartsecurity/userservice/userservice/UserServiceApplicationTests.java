84f99f36a39e5ecb5dc5285227cb42051b5eb0bd
Andres Bolivar <andresfebo187@gmail.com>
2021-11-26 15:25:49 -0500

    first commit

---
+++ user-service/src/test/java/com/smartsecurity/userservice/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartsecurity.userservice.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
